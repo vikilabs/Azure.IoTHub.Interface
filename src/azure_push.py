784df564a15e9b58a239e12fa5c71c89b2ec2a0e
‘vikilabs’ <vickyemailbox@gmail.com>
2019-10-13 23:34:09 +0200

    message payload json

--- src/azure_push.py
+++ src/azure_push.py
@@ -2,6 +2,7 @@ import iothub_client
 from iothub_client import IoTHubMessage, IoTHubMessageDispositionResult, IoTHubError, DeviceMethodReturnValue
 from iothub_client import IoTHubClient, IoTHubClientError, IoTHubTransportProvider, IoTHubClientResult
 import time
+import json
 
 communication_protocol = IoTHubTransportProvider.MQTT
 
@@ -10,12 +11,9 @@ connection_string = "HostName=xxxx;DeviceId=xxx;SharedAccessKey=xxxx"
 device_id="ESP8266"
 messageId=1
 
-message_format = '{\"deviceId\": %s,\"messageId\": %d, \"aZ\": %f, \"gps_lat_dir\" : %s, \"gps_lat_val\": %s, \"gps_lon_dir\":%s, \"gps_lon_val\":%s}'
-
 client=None
 request_count = 0
 response_count = 0
-ack_received=False
 
 def iothub_message_callback(message, result, user_context):
     global response_count
@@ -31,7 +29,15 @@ def iothub_client_init():
         print "[ IoT Client ] Initialization Failed"
 
 def frame_message(device_id, message_id, sensor_value, gps_lat, gps_lon):
-    msg_payload = message_format % (device_id, message_id, sensor_value, "N", gps_lat, "E", gps_lon)
+    data = {}
+    data['deviceId'] = device_id 
+    data['messageId'] = message_id
+    data['aZ'] = sensor_value
+    data['gps_lat_dir'] = "N"
+    data['gps_lat_val'] = gps_lat
+    data['gps_lon_dir'] = "E"
+    data['gps_lon_val'] = gps_lon
+    msg_payload = json.dumps(data)
     return msg_payload 
 
 def iothub_client_send(msg_payload):
